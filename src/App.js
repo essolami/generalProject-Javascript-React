c16a7d4d737ea5bf59762f81809d548cea3c7430
essolami <37643427+essolami@users.noreply.github.com>
2023-02-14 20:05:46 +0100

    1 file modified, 6 added, 6 deleted

--- src/App.js
+++ src/App.js
@@ -1,19 +1,13 @@
 import React from 'react';
 import './style.css';
-import Exercice1 from './exercices/exercice1.js';
-import Exercice2 from './exercices/exercice2.js';
-import Exercice3 from './exercices/exercice3.js';
-import Exercice4 from './exercices/exercice4.js';
-import { Exercice5 } from './exercices/exercice5.js';
+import Exercice1 from './javaScript/exercice1.js';
+import Exercice2 from './javaScript/exercice2.js';
+import Exercice3 from './javaScript/exercice3.js';
+import Exercice4 from './javaScript/exercice4.js';
+import { Exercice5 } from './javaScript/exercice5.js';
 import ErrorBoundary from './react/errorBoundary/errorBoundary';
 // import ExerciceReact1 from './react';
 export default function App() {
-  const undefinedVariable = () => {
-    // if (this.state.counter === 5) {
-    // Simulate a JS error
-    throw new Error('I crashed!');
-    // }
-  };
   return (
     <div>
       {/* <Exercice1/> */}

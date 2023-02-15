b085b1a9c1537383b79816ba835aee48d995725f
essolami <37643427+essolami@users.noreply.github.com>
2023-02-15 20:02:41 +0100

    6 files modified

--- src/react/redux/store/cart-actions.js
+++ src/react/redux/store/cart-actions.js
@@ -5,7 +5,7 @@ export const fetchCartData = () => {
   return async (dispatch) => {
     const fetchData = async () => {
       const response = await fetch(
-        'https://react-http-6b4a6.firebaseio.com/cart.json'
+        'https://javascript-react-guide-default-rtdb.firebaseio.com/cart.json'
       );
 
       if (!response.ok) {

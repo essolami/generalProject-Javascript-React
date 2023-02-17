a8805de2f2674e80afb15b4825387fb90587e539
essolami <37643427+essolami@users.noreply.github.com>
2023-02-17 20:27:10 +0100

    1 file modified

--- src/react/redux/store/cart-actions.js
+++ src/react/redux/store/cart-actions.js
@@ -24,44 +24,44 @@ export const addItemToCart = createAsyncThunk(
   'users/fetchCartData',
   async (_, thunkAPI) => {
     console.log('heeeey');
-    try {
-      thunkAPI.dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!',
-        })
-      );
+    // try {
+    //   thunkAPI.dispatch(
+    //     uiActions.showNotification({
+    //       status: 'pending',
+    //       title: 'Sending...',
+    //       message: 'Sending cart data!',
+    //     })
+    //   );
 
-      const response = await fetch(`${url}/cart.json`, {
-        method: 'PUT',
-        body: JSON.stringify({
-          items: cart.items,
-          totalQuantity: cart.totalQuantity,
-        }),
-      });
+    //   const response = await fetch(`${url}/cart.json`, {
+    //     method: 'PUT',
+    //     body: JSON.stringify({
+    //       items: cart.items,
+    //       totalQuantity: cart.totalQuantity,
+    //     }),
+    //   });
 
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
+    //   if (!response.ok) {
+    //     throw new Error('Sending cart data failed.');
+    //   }
 
-      thunkAPI.dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!',
-        })
-      );
-      // const data = await response.json();
-      return await response.json();
-    } catch (error) {
-      thunkAPI.dispatch(
-        uiActions.showNotification({
-          status: 'error',
-          title: 'Error!',
-          message: 'Sending cart data failed!',
-        })
-      );
-    }
+    //   thunkAPI.dispatch(
+    //     uiActions.showNotification({
+    //       status: 'success',
+    //       title: 'Success!',
+    //       message: 'Sent cart data successfully!',
+    //     })
+    //   );
+    //   // const data = await response.json();
+    //   return await response.json();
+    // } catch (error) {
+    //   thunkAPI.dispatch(
+    //     uiActions.showNotification({
+    //       status: 'error',
+    //       title: 'Error!',
+    //       message: 'Sending cart data failed!',
+    //     })
+    //   );
+    // }
   }
 );

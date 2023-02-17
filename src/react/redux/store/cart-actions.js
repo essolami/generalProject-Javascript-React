b68f4ef5a96d89c2eb6823d70cb8f55f98698fc4
essolami <37643427+essolami@users.noreply.github.com>
2023-02-17 18:23:26 +0100

    1 file modified

--- src/react/redux/store/cart-actions.js
+++ src/react/redux/store/cart-actions.js
@@ -1,20 +1,11 @@
 import { uiActions } from './ui-slice';
-import { cartActions } from './cart-slice';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-import axios from 'axios';
 const url = 'https://javascript-react-guide-default-rtdb.firebaseio.com';
 export const fetchCartData = createAsyncThunk(
   'users/fetchUser',
-  async (data, thunkAPI) => {
-    // console.log(data);
-    // console.log(thunkApi);
+  async (_, thunkAPI) => {
     try {
-      // console.log(name);
-      // console.log(thunkAPI);
-      // console.log(thunkAPI.getState());
-      // thunkAPI.dispatch(openModal());
-      const response = await axios.get(`${url}/cart.json`);
-      console.log(response);
+      const response = await fetch(`${url}/cart.json`);
       const data = await response.json();
       return data;
     } catch (error) {
@@ -29,76 +20,42 @@ export const fetchCartData = createAsyncThunk(
   }
 );
 
-// export const fetchCartData = () => {
-//   return async (dispatch) => {
-//     const fetchData = async () => {
-//       const response = await fetch(
-//         'https://javascript-react-guide-default-rtdb.firebaseio.com/cart.json'
-//       );
-
-//       if (!response.ok) {
-//         throw new Error('Could not fetch cart data!');
-//       }
-
-//       const data = await response.json();
-
-//       return data;
-//     };
-
-//     try {
-//       const cartData = await fetchData();
-//       dispatch(cartActions.replaceCart(cartData));
-//     } catch (error) {
-//       dispatch(
-//         uiActions.showNotification({
-//           status: 'error',
-//           title: 'Error!',
-//           message: 'Fetching cart data failed!',
-//         })
-//       );
-//     }
-//   };
-// };
-
-export const sendCartData = (cart) => {
-  return async (dispatch) => {
-    dispatch(
-      uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!',
-      })
-    );
-
-    const sendRequest = async () => {
-      const response = await fetch(
-        'https://javascript-react-guide-default-rtdb.firebaseio.com/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify({
-            items: cart.items,
-            totalQuantity: cart.totalQuantity,
-          }),
-        }
+export const sendCartData = createAsyncThunk(
+  'users/fetchUser',
+  async (_, thunkAPI) => {
+    console.log('heeeey');
+    try {
+      thunkAPI.dispatch(
+        uiActions.showNotification({
+          status: 'pending',
+          title: 'Sending...',
+          message: 'Sending cart data!',
+        })
       );
+
+      const response = await fetch(`${url}/cart.json`, {
+        method: 'PUT',
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity,
+        }),
+      });
 
       if (!response.ok) {
         throw new Error('Sending cart data failed.');
       }
-    };
 
-    try {
-      await sendRequest();
-
-      dispatch(
+      thunkAPI.dispatch(
         uiActions.showNotification({
           status: 'success',
           title: 'Success!',
           message: 'Sent cart data successfully!',
         })
       );
+      // const data = await response.json();
+      return await response.json();
     } catch (error) {
-      dispatch(
+      thunkAPI.dispatch(
         uiActions.showNotification({
           status: 'error',
           title: 'Error!',
@@ -106,5 +63,5 @@ export const sendCartData = (cart) => {
         })
       );
     }
-  };
-};
+  }
+);
